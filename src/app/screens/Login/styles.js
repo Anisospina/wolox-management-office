db3d684cbdbb117394b0830f7861aa7bc9f6be6f
Anisospina <anisospina@gmail.com>
2019-10-24 14:29:00 -0500

    added const

--- src/app/screens/Login/styles.js
+++ src/app/screens/Login/styles.js
@@ -1,27 +1,29 @@
 import {StyleSheet} from 'react-native';
 import {green, blue, transparent, aquaHaze} from '@constants/colors';
+import {refRatioScale} from '@constants/platform';
+import {MARGIN_PADDING, MARGIN_PADDING_DOUBLE} from '@constants/dimensions';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: aquaHaze,
+    backgroundColor: aquaHaze
   },
   formElementContainer: {
     backgroundColor: transparent,
     borderBottomColor: blue,
-    borderBottomWidth: 1,
+    borderBottomWidth: 1
   },
   formElement: {
     padding: 3,
-    height: 30,
-    width: 200,
+    height: refRatioScale(30),
+    width: refRatioScale(200)
   },
   formButton: {
     backgroundColor: green,
-    padding: 10,
+    padding: MARGIN_PADDING,
     borderRadius: 3,
-    margin: 20,
-  },
+    margin: MARGIN_PADDING_DOUBLE
+  }
 });
